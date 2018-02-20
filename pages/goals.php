556c93746f23db382bbaf06833dcd07cc6186667
skurtz00 <35576480+skurtz00@users.noreply.github.com>
2018-02-20 16:03:04 -0500

    Add files via upload

---
+++ pages/goals.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>FLEX</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="style.css">
+    </head>
+    
+    <body>
+        <header>
+            <a href="home.html"><img src="antlers.png" alt="logo" height="50px" width="50px"/></a>
+        <nav>
+        <ul>
+            <li class="dropdown">
+                <a href="javascript:void(0)" class="dropbtn">Menu</a>
+                <div class="dropdown-content">
+                <a href="#">Profile</a>
+                <a href="#">Synchronize</a>
+                <a href="#">Tracking</a>
+                <a href="#">Goals</a>
+                <a href="#">Reports</a>
+                </div>
+            </li>
+        </ul>
+        </nav>
+        </header>
+        <main>
