2fe2509aeb0e00e57f4f0e82c2746362140bec12
jcuevas23 <35585753+jcuevas23@users.noreply.github.com>
2018-03-07 10:58:38 -0500

    testlogin initial

--- testlogin.php
+++ testlogin.php
@@ -1,43 +1,29 @@
 <?php require("login.php"); ?>
 
-<!DOCTYPE html>
-<html>
+<!doctype html>
+<html lang="en">
     <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>FLEX</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="style.css">
-        <link rel="stylesheet" href="homestyle.css">
-        <link rel="stylesheet" href="css/styles.css?v=1.0">
+    <meta charset="utf-8">
+
+    <title>The HTML5 Herald</title>
+    <meta name="description" content="The HTML5 Herald">
+    <meta name="author" content="SitePoint">
+
+    <link rel="stylesheet" href="css/styles.css?v=1.0">
+
+    <!--[if lt IE 9]>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+    <![endif]-->
     </head>
-    
+
     <body>
-        <header>
-            <a href="home.html"><img src="antlers.png" alt="logo" height="50px" width="50px"/></a>
-        </header>
-        <main>
-	       <h1>
-	           FLEX    
-	       </h1>
-	       <script src="js/scripts.js"></script>
-	        <form method="POST" action="">
-	            login test<br>
-	            <input type="text" name="formUser"><br>
-	            <input type="text" name="formPass"><br>
-	            <input type="submit" value="submit"><br>
-	            <?php echo $message."<br>";?>
-	        </form>
-        </main>
-        
-        <footer>
-            <br>
-            <div style="float:left; display: inline;">&copy; 2018 <br>Fairfield University <br>School of Nursing</div>
-            <div style="float: right; display: inline">
-            1073 North Benson Road
-            <br>Fairfield, CT 06824
-            </div>
-        </footer>
+        <script src="js/scripts.js"></script>
+        <form method="POST" action="">
+            login test<br>
+            <input type="text" name="formUser"><br>
+            <input type="text" name="formPass"><br>
+            <input type="submit" value="submit"><br>
+            <?php echo $message."<br>";?>
+        </form>
     </body>
 </html>
-
