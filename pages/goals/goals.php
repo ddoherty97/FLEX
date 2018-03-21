1e62fb4850d2fc452353bdc3a7303c8d68d1088b
jcuevas23 <35585753+jcuevas23@users.noreply.github.com>
2018-03-21 14:38:17 -0400

    relative links

--- pages/goals/goals.php
+++ pages/goals/goals.php
@@ -29,12 +29,12 @@
         <br>
         <main style="font-size: 20px; font-weight: bold;">
             <img src="../../images/stag.png" alt="Stag" height="100px" width="100px"><br><br>
-            <a href="fitnessgoals.html" style="text-decoration: none; ">Fitness Goals</a><br><br>
-            <a href="dietarygoals.html" style="text-decoration: none; ">Dietary Goals</a><br><br>
-            <a href="socialgoals.html" style="text-decoration: none; ">Social Goals</a><br><br>
-            <a href="mentalgoals.html" style="text-decoration: none; ">Mental Goals</a><br><br>
-            <a href="spiritualgoals.html" style="text-decoration: none; ">Spiritual Goals</a><br><br>
-            <a href="screentimegoals.html" style="text-decoration: none; ">Screen Time Goals</a><br><br>
+            <a href="fitnessgoals.php" style="text-decoration: none; ">Fitness Goals</a><br><br>
+            <a href="dietarygoals.php" style="text-decoration: none; ">Dietary Goals</a><br><br>
+            <a href="socialgoals.php" style="text-decoration: none; ">Social Goals</a><br><br>
+            <a href="mentalgoals.php" style="text-decoration: none; ">Mental Goals</a><br><br>
+            <a href="spiritualgoals.php" style="text-decoration: none; ">Spiritual Goals</a><br><br>
+            <a href="screentimegoals.php" style="text-decoration: none; ">Screen Time Goals</a><br><br>
         </main>
                 <footer>
             <br>
