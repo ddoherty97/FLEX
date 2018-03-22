56b07215793b897f9ad8240ac700a2b0e4251837
ddoherty97 <davis.doherty@student.fairfield.edu>
2018-03-21 21:30:36 -0400

    updated front page to secure it and add error messages

--- pages/goals/screentimegoals.php
+++ pages/goals/screentimegoals.php
@@ -1,3 +1,26 @@
+<?php
+    //check if session is already running
+	if(!isset($_SESSION)) 
+    { 
+        session_start();
+    }//end if
+	
+	//if no session is active, redirect to login page
+    $phpFolderPath = "../../php/";
+    $logoutFile = $phpFolderPath."logout.php";
+    require($phpFolderPath."IsLoggedIn.php");
+
+    //get result of last goal creation
+    if(isset($_GET['s']))
+    {
+        $result = $_GET['s'];
+    }//end if
+    else
+    {
+        $result = "none";
+    }//end else
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -28,26 +51,39 @@
         </header>
         <main>
         	<h1>FLEX</h1>
-        	<h2>Screen Time Goal</h2>
+        	<br>
+            <h2>Screen Time Goal</h2>
+
+            <?php
+                //display status of last goal creation result
+                if($result=="f")
+                {
+            ?>
+                    <div style="color: red; padding: 20px;">THERE WAS AN ERROR CREATING YOUR GOAL.</div>
+            <?php
+                }//end if
+                else if($result=="s")
+                {
+            ?>
+                    <div style="color: green; padding: 20px;">Screen Time Goal Created!</div>
+            <?php
+                }//end if
+            ?>
         	
-        	<form method="POST" action=""> <!-- add php -->
-        	
-        	<label for="screenTimeGoal">Daily Screen Time Goal<sup>*</sup>: </label>
-				<input type="text" name="screenTimeGoal" id="screenTimeGoal" size="5"> Minutes<br><br>
-			
-			<button type="submit"  value="Submit">Submit</button>
-			
-			</form>
-							
+        	<form method="POST" action="../../php/CreateScreenTimeGoal.php">
+                <label for="screenTimeGoal">Daily Screen Time Goal<sup>*</sup>: </label>
+                    <input type="text" name="screenTimeGoal" id="screenTimeGoal" size="5"> Minutes<br><br>
+                
+                <button type="submit"  value="Submit">Submit</button>
+			</form>						
         </main>
         <footer>
             <br>
             <div style="float:left; display: block;">&copy; 2018 <br>Fairfield University <br>School of Nursing</div>
             <div style="float: right; display: block">
-            1073 North Benson Road
+            <br>1073 North Benson Road
             <br>Fairfield, CT 06824
             </div>
-        </footer>
-            
+        </footer>    
     </body>
 </html>
