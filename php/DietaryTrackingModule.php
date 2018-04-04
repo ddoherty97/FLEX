dd7041f5d0594e7f742268de3e27af69e2c7b791
ddoherty97 <davis.doherty@student.fairfield.edu>
2018-04-04 16:09:23 -0400

    debugging

--- php/DietaryTrackingModule.php
+++ php/DietaryTrackingModule.php
@@ -53,10 +53,14 @@
             $submitted = date("Y-m-d H:i:s");
 
             //build SQL to insert data into database
-			$sql = "INSERT INTO DIETARY_DATA    (DIET_DATA_OWNER, DIET_DATE, DIET_TITLE, DIET_TIMESTAMP, DIET_CALORIES, DIET_WATER)";
-			echo "between";
+			$sql = "INSERT INTO DIETARY_DATA (DIET_DATA_OWNER, DIET_DATE, DIET_TITLE, DIET_TIMESTAMP, DIET_CALORIES, DIET_WATER)";
+            
+            echo "between sql<br>";
+
+            $sql2 = " VALUES ('$this->dataOwner', '$date', '$typeOfFood', ";//'$submitted', ";
+
+            echo "after sql";
 			exit();
-            $sql = $sql."VALUES 					    ('$this->dataOwner', '$date', '$typeOfFood', '$submitted', ";
 			
                     //if no calories consumed
                     if($calories == "0")
