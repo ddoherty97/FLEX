dc841fe7e00c2aedb987d6a1f4f183714fb1204e
jcuevas23 <35585753+jcuevas23@users.noreply.github.com>
2018-04-10 17:54:18 -0400

    mental report

---
+++ php/MentalEntry.php
@@ -0,0 +1,176 @@
+<?php
+	/**
+     * Mental Entry (MentalEntry.php)
+     * A mental entry object represents a row in the MENTAL_DATA table which contains
+     *      all the information for a single entry made using the Mental Tracking Module.
+     *      It's purpose is to be used in the Mental Report Module as a concise way to
+     *      represent a mental "event".
+     * Author: Jaclyn Cuevas
+     * Last Updated: 4/10/18 JC
+     **/
+
+    class MentalEntry
+    {
+        private $entryID;           	//ID of the mental entry in the database
+        private $entryDate;         	//date of mental entry
+        private $counselingDuration;	//time spent at counseling session
+        private $counselingType;		//type of counseling attended
+		private $counselingNotes;		//notes for counseling session
+        private $stressLevel;       	//stress level of user
+        private $stressFactors;			//factors contributing to stress
+        private $entrySubmitted;    	//submitted date of entry
+
+         /**
+         * __construct()
+         * This method creates mental entry object with immutable attributes
+         * Parameters:  $ID->ID of the mental entry in the database
+         *              $date->date of mental entry
+         *              $duration->duration of counseling session
+         *              $type->type of counseling session
+         *              $notes->notes for counseling session
+		 * 				$level->stress level
+		 *				$factors->stress factors 
+         *              $submitted->submitted date of entry
+         * Exceptions: none
+         **/
+        function __construct($ID, $date, $duration, $type, $notes, $level, $factors, $submitted)
+        {
+            //if no duration provided
+            if($duraiton=="")
+            {
+                $duration = "0";
+            }//end if
+
+            //if no type provided
+            if($type == "")
+            {
+                //assign new title
+                $type = "Only Stress Level Provided";
+            }//end if
+
+            //if no notes provided
+            if($notes == "")
+            {
+                $notes = "No Notes Provided";
+            }//end if    
+            
+             //if no stress level provided
+            if($level == "")
+            {
+                $level = 0;
+            }//end if      
+            
+             //if no stress factors provided
+            if($factors == "")
+            {
+                $factors = "No Stress Factors Provided";
+            }//end if    
+            
+            //assign constructor params to member variables
+            $this->entryID = $ID;
+            $this->entryDate = $date;
+            $this->counselingDuration = $duration;
+            $this->counselingType = $type;
+            $this->counselingNotes = $notes;
+			$this->stressLevel = $level;
+			$this->stressFactors = $factors;
+            $this->entrySubmitted = $submitted;
+        }//close constructor
+
+        /**
+         * getEntryID()
+         * This method gets the ID of the mental entry in the database
+         * Parameters:  none
+         * Returns: ID of the mental entry in the database
+         * Exceptions: none
+         **/
+        function getEntryID()
+        {
+            return $this->entryID;
+        }//close getEntryID
+
+        /**
+         * getEntryDate()
+         * This method gets the date of mental entry
+         * Parameters:  none
+         * Returns: date of mental entry
+         * Exceptions: none
+         **/
+        function getEntryDate()
+        {
+            return $this->entryDate;
+        }//close getEntryDate
+
+        /**
+         * getDuration()
+         * This method gets the duration of the counseling session
+         * Parameters:  none
+         * Returns: duration of counseling session
+         * Exceptions: none
+         **/
+        function getDuration()
+        {
+            return $this->counselingDuration;
+        }//close getDuration
+
+        /**
+         * getType()
+         * This method gets the type of counseling session
+         * Parameters:  none
+         * Returns: type of counseling session
+         * Exceptions: none
+         **/
+        function getType()
+        {
+            return $this->counselingType;
+        }//close getType
+
+        /**
+         * getNotes()
+         * This method gets the notes on the counseling session
+         * Parameters:  none
+         * Returns: notes on counseling session
+         * Exceptions: none
+         **/
+        function getNotes()
+        {
+            return $this->counselingNotes;
+        }//close getNotes
+        
+         /**
+         * getLevel()
+         * This method gets the stress level
+         * Parameters:  none
+         * Returns: stress level
+         * Exceptions: none
+         **/
+        function getLevel()
+        {
+            return $this->stressLevel;
+        }//close getLevel
+        
+         /**
+         * getFactors()
+         * This method gets the factors for the stress level
+         * Parameters:  none
+         * Returns: factors for stress level
+         * Exceptions: none
+         **/
+        function getFactors()
+        {
+            return $this->stressFactors;
+        }//close getFactors
+
+        /**
+         * getTimestamp()
+         * This method gets the submitted date of entry
+         * Parameters:  none
+         * Returns: submitted date of entry
+         * Exceptions: none
+         **/
+        function getTimestamp()
+        {
+            return $this->entrySubmitted;
+        }//close getTimestamp
+    }//close MentalEntry
+?>
---
+++ php/MentalReport.php
@@ -0,0 +1,128 @@
+<?php
+	/**
+     * Mental Report (MentalReport.php)
+     * A mental report object represents all the mental data available including all
+     *      entries in the database, goals to complete, and completion progress of the goals
+     * Author: Jaclyn Cuevas
+     * Last Updated: 4/10/18 JC
+     **/
+
+    class MentalReport
+    {
+        private $counselingGoals;		//counseling goals
+        private $stressGoals;        	//stress goals
+        private $counselingProgresses;	//completion progress of all counseling goals
+        private $stressProgresses;		//completion progress of all stress goals
+        private $entries;           	//all mental entries
+        private $startDate;         	//start date of the report
+        private $endDate;           	//end date of the report
+
+        /**
+         * __construct()
+         * This method creates a mental report with immutable attributes
+         * Parameters:  $counselingGoalArray->all counseling goals
+         *              $stressGoalArray->all stress goals
+         *              $counselingProgressArray->completion progress of all counseling goals
+         *              $stressProgressArray->completion progress of all stress goals
+         *              $entryArray->all mental entries
+         *              $startDate->start date of the report
+         *              $endDate->end date of the report
+         * Exceptions: none
+         **/
+        function __construct($counselingGoalArray, $stressGoalArray, $counselingProgressArray, $stressProgressArray, $entryArray, $startDate, $endDate)
+        {
+            //assign constructor params to member variables
+            $this->counselingGoals = $counselingGoalArray;
+            $this->stressGoals = $stressGoalArray;
+            $this->counselingProgresses = $counselingProgressArray;
+            $this->stressProgresses = $stressProgressArray;
+            $this->entries = $entryArray;
+            $this->startDate = $startDate;
+            $this->endDate = $endDate;
+        }//close constructor
+
+        /**
+         * getCounselingGoals()
+         * This method gets the active counseling goals
+         * Parameters:  none
+         * Returns: active counseling goals
+         * Exceptions: none
+         **/
+        function getCounselingGoals()
+        {
+            return $this->counselingGoals;
+        }//close getCounselingGoals
+
+        /**
+         * getStressGoals()
+         * This method gets the active stress level goals
+         * Parameters:  none
+         * Returns: active stress level goals
+         * Exceptions: none
+         **/
+        function getStressGoals()
+        {
+            return $this->stressGoals;
+        }//close getStressGoals
+
+        /**
+         * getCounselingProgresses()
+         * This method gets the progress of all active counseling goals
+         * Parameters:  none
+         * Returns: progress of all active counseling goals
+         * Exceptions: none
+         **/
+        function getCounselingProgresses()
+        {
+            return $this->counselingProgresses;
+        }//close getCcounselingProgresses
+
+        /**
+         * getWaterProgresses()
+         * This method gets the progress of all active water goals
+         * Parameters:  none
+         * Returns: progress of all active water goals
+         * Exceptions: none
+         **/
+        function getStressProgresses()
+        {
+            return $this->stressProgresses;
+        }//close getStressProgresses
+
+        /**
+         * getMentalEntries()
+         * This method gets all mental entries between the report start date and end date
+         * Parameters:  none
+         * Returns: mental entries between the report start date and end date
+         * Exceptions: none
+         **/
+        function getMentalEntries()
+        {
+            return $this->entries;
+        }//close getMentalEntries
+
+        /**
+         * getStartDate()
+         * This method gets the start date of the mental report
+         * Parameters:  none
+         * Returns: start date of the mental report
+         * Exceptions: none
+         **/
+        function getStartDate()
+        {
+            return $this->startDate;
+        }//close getStartDate
+
+        /**
+         * getEndDate()
+         * This method gets the end date of the mental report
+         * Parameters:  none
+         * Returns: end date of the mental report
+         * Exceptions: none
+         **/
+        function getEndDate()
+        {
+            return $this->endDate;
+        }//close getendDate
+    }//close MentalReport
+?>
