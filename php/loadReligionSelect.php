5d2db3cc1ead25b912c3858bd6e3812378469b81
ddoherty97 <davis.doherty@student.fairfield.edu>
2018-04-11 21:45:02 -0400

    updated religion list

--- php/loadReligionSelect.php
+++ php/loadReligionSelect.php
@@ -4,18 +4,16 @@
      * This file outputs option elements of a select field containing
      *      numerous religions in HTML format
      * Author: Davis Doherty
-     * Last Updated: 3/4/18 DD
+     * Last Updated: 4/11/18 DD
      **/
 ?>
 
 <option value="-1">Select</option>
-<option value="0">R0</option>
-<option value="1">R1</option>
-<option value="2">R2</option>
-<option value="3">R3</option>
-<option value="4">R4</option>
-<option value="5">R5</option>
-<option value="6">R6</option>
-<option value="7">R7</option>
-<option value="8">R8</option>
-<option value="9">R9</option>
+<option value="Atheism">Atheism</option>
+<option value="Buddism">Buddism</option>
+<option value="Catholicism">Catholicism</option>
+<option value="Hinduism">Hinduism</option>
+<option value="Islam">Islam</option>
+<option value="Judaism">Judaism</option>
+<option value="Protestantism">Protestantism</option>
+<option value="Other">Other</option>
