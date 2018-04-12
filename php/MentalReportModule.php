39838540ea518560970cd7f74c13fe7205b807d0
ddoherty97 <davis.doherty@student.fairfield.edu>
2018-04-11 22:02:04 -0400

    finished mental report module

--- php/MentalReportModule.php
+++ php/MentalReportModule.php
@@ -9,6 +9,8 @@
 
     require_once("CommunicationModule.php");
     require_once("MentalGoalModule.php");
+    require_once("MentalEntry.php");
+    require_once("MentalReport.php");
 	
 	class MentalReportModule
 	{
@@ -137,6 +139,56 @@
         }//close getStressLevelProgress
 
         /**
+         * getMentalEntries()
+         * This method builds an array of all mental health entries
+         * Parameters:  $startDate->date to start selecting entries
+         *              $endDate->date to stop selecting entries
+         * Returns: array of MentalEntry objects
+         * Exceptions: none
+         **/
+        function getMentalEntries($startDate, $endDate)
+        {
+            //array to contain all mental health entries
+            $mentalEntries = [];
+            $entryIndex = 0;
+            
+            //format date objects for use in SQL
+            $startDateDisplay = date_format($startDate,'Y-m-d H:i:s');
+            $endDateDisplay = date_format($endDate,'Y-m-d H:i:s');
+
+            //get all mental health data points in data range
+            $mentalSQL = "SELECT *
+                          FROM MENTAL_DATA
+                          WHERE MENTAL_DATA_OWNER='$this->dataOwner'
+                          AND MENTAL_COUNSELING_DATE BETWEEN '$startDateDisplay' AND '$endDateDisplay'";
+            $entries = $this->comMod->queryDatabase($mentalSQL);
+
+            //read all mental health entries, create objects, and add to array
+            while($currEntry = mysqli_fetch_array($entries))
+            {
+                //get details from database
+                $id = $currEntry['MENTAL_DATA_ID'];
+                $date = $currEntry['MENTAL_COUNSELING_DATE'];
+                $counselingDuration = $currEntry['MENTAL_COUNSELING_DURATION'];
+                $counselingType = $currEntry['MENTAL_COUNSELING_NOTES'];
+                $counselingNotes = $currEntry['MENTAL_STRESS_OTHER'];
+                $stressLevel = $currEntry['MENTAL_STRESS_LEVEL'];
+                $stressFactors = $currEntry['MENTAL_STRESS_FACTORS'];
+                $timestamp = $currEntry['MENTAL_DATA_TIMESTAMP'];
+
+                //create mental entry object
+                $entry = new MentalEntry($id, date_create($date), $counselingDuration, $counselingType, $counselingNotes, $stressLevel, $stressFactors, $timestamp);
+
+                //add object to array
+                $dietaryEntries[$entryIndex] = $entry;
+                $entryIndex++;
+            }//end while
+
+            //return array of entries
+            return $dietaryEntries;
+        }//close getDietaryEntries
+
+        /**
          * getMentalReport()
          * This method builds a report of all the mental health related activities
          * Parameters:  $startDate->date to start selecting to add in the report
@@ -146,7 +198,24 @@
          **/
         function getMentalReport($startDate, $endDate)
         {
+            //get all counseling goals
+            $cGoals = $this->mentalMod->getCounselingGoals();
 
+            //get all counseling goal progresses
+            $cProgress = $this->getCounselingSessionProgress();
+
+            //get all stress goals
+            $sGoals = $this->mentalMod->getStressGoals();
+
+            //get all stress goal progresses
+            $sProgress = $this->getStressLevelProgress();
+
+            //get all mental health entries
+            $entries = $this->getMentalEntries($startDate, $endDate);
+
+            //build and return mental health report object
+            $report = new MentalReport($cGoals, $sGoals, $cProgress, $sProgress, $entries, $startDate, $endDate);
+            return $report;
         }//close getMentalReport
     }//close MentalReportModule
 ?>
